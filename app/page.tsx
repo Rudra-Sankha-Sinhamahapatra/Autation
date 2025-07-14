f419409f85b0bbb03d6a3a9f2d47f8f546f03384
Rudra-Sankha-Sinhamahapatra <rssmp120@gmail.com>
2025-07-14 06:58:20 +0530

    added: animation card

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { ContainerScroll } from "@/components/global/container-scroll-animation";
 import Navbar from "@/components/global/navbar";
+import { TitleComponent } from "@/components/home/title-component";
 
 export default function Home() {
   return (
-  <main>
-    <Navbar />
-  </main>
+    <main>
+      <Navbar />
+      <section className="h-screen w-full  bg-neutral-950 rounded-md  !overflow-visible relative flex flex-col items-center  antialiased">
+        <div className="absolute inset-0  h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_35%,#223_100%)]"></div>
+        <div className="flex flex-col mt-[-100px] md:mt-[-50px]">
+          <ContainerScroll
+            titleComponent={<TitleComponent />}
+          />
+        </div>
+      </section>
+      
+    </main>
   );
 }
